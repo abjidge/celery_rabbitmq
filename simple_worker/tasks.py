2e2e88b7552d6ca0d1d57b13f9838ebf93b07dc4
Abhay Jidge <abjidge@gmail.com>
2022-07-05 17:14:30 +0530

    asdas

--- simple_worker/tasks.py
+++ simple_worker/tasks.py
@@ -8,10 +8,11 @@ app = Celery('tasks',
              broker='amqp://admin:password@localhost:5672',
              backend='rpc://')
 
+app.conf.task_ignore_result = True
 
-@app.task()
+@app.task(ignore_result=True)
 def longtime_add(x, y):
     logger.info('Got Request - Starting work ')
-    time.sleep(4)
+    # time.sleep(4)
     logger.info('Work Finished ')
     return x + y
